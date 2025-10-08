3bc9159e6bb4e88feb55c366dc28662a2ba5c524
ansh0712jul <agrawalansh624@gmail.com>
2025-10-08 14:22:54 +0530

    Connect: database

---
+++ backend/src/constant.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "Route_Optimisation"
---
+++ backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+
+const connectDB = async () => {
+    try {
+
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}` )
+        console.log(`mongo db connection established 🥳🥳  ${connectionInstance.connection.host}`);
+
+
+    } catch (error) {
+        console.log("Error while connecting to MongoDB", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
