4e6c35e3cd8125c9fbf7391d796d4e8f51c56902
Hariom Sharma <hariomsharmaji70@gmail.com>
2025-10-13 14:44:50 +0530

    add admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import LandingPage from "./pages/LandingPage";
+import AdminDashboard from "./pages/AdminDashboard";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AuthPage />} />
       <Route path="/home" element={<LandingPage />} />
+      <Route path="/admin" element={<AdminDashboard />} />
     </Routes>
   );
 };
