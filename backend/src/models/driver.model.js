e04e4cf8f21b2ab8c8e47a9ddf9c885136dc9c43
Ishika Goyal <goyalishika2004@gmail.com>
2025-10-24 22:20:42 +0530

    Db models added

---
+++ backend/src/models/driver.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const driverSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    phone: { type: String },
+    vehicleId: { type: mongoose.Schema.Types.ObjectId, ref: "Vehicle" },
+    routeIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Route",
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["free", "busy", "offline"],
+      default: "free",
+    },
+    currentLocation: {
+      lat: { type: Number },
+      lng: { type: Number },
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Driver", driverSchema);
