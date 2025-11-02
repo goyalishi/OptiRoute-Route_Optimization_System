01f7beed8e4f6cbf4748bab52cc02ccd21194b50
Hariom Sharma <hariomsharmaji70@gmail.com>
2025-11-02 14:39:49 +0530

    admin/driver Controller

---
+++ backend/src/controllers/admin.controller.js
@@ -0,0 +1,71 @@
+import { Admin } from "../models/admin.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+// ---------- SIGNUP ----------
+export const adminSignup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+    
+    const newAdmin = new Admin({ username, email, password });
+    await newAdmin.save();
+
+    res.status(201).json({
+      message: "Admin registered successfully",
+      user: {
+        id: newAdmin._id,
+        username: newAdmin.username,
+        email: newAdmin.email,
+      },
+      
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Signup failed", error: error.message });
+  }
+};
+
+// ---------- LOGIN ----------
+export const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "Email and password required" });
+
+    const admin = await Admin.findOne({ email });
+    if (!admin) return res.status(404).json({ message: "Admin not found" });
+
+    const isMatch = await admin.isPasswordCorrect(password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid email or password" });
+
+    const accessToken = admin.generateAccessToken();
+    const refreshToken = admin.generateRefreshToken();
+
+    admin.refrreshToken = refreshToken;
+    await admin.save();
+
+    res.status(200).json({
+      message: "Login successful",
+      role: "admin", // 🔹 Add this field
+      accessToken,
+      refreshToken,
+      user: {
+        id: admin._id,
+        username: admin.username,
+        email: admin.email,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error: error.message });
+  }
+};
+
